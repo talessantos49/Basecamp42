39573cb9421c7c1434a94e2502ebc0776472fdea
talessantos49 <talessantos49@gmail.com>
2022-09-19 11:10:43 -0300

    Primeiros testes com a função hook e main

--- So_long/Projetos-42/so_long/so_long/hook.c
+++ So_long/Projetos-42/so_long/so_long/hook.c
@@ -1,17 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   hook.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tasantos <tasantos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 14:37:22 by tasantos          #+#    #+#             */
-/*   Updated: 2022/08/19 23:40:30 by tasantos         ###   ########.fr       */
+/*   Updated: 2022/09/19 11:10:02 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+#define MLX_ERROR 1
+#define WINDOW_HEIGHT 400
+#define WINDOW_WIDTH 600
+/*
 typedef struct	s_vars {
 	void	*mlx;
 	void	*win;
@@ -23,6 +27,7 @@ int	key_hook(int keycode, t_vars *vars)
 	codigos que apareceram quando apertei a tecla W:119 s:115 a:97 d:100 
 	Seta pra cima: 65362	Seta pra baixo:	65364	Seta pra direita:	65363	Seta Pra esquerda: 65361
 */
+/*
 	printf("Keycode: %d\n", keycode);
 	printf("Keycode: %i\n", keycode);
 	printf("Hello from key_hook!\n");
@@ -37,4 +42,113 @@ int	main(void)
 	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
 	mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_loop(vars.mlx);
+}
+
+*/
+/*
+#include <X11/keysym.h>
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_data;
+
+int	handle_no_event(void *data)
+{
+	/* This function needs to exist, but it is useless for the moment 
+	return (0);
+}
+
+int	handle_input(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	if (keysym == XK_Q)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
+			"My first window!");
+	if (data.win_ptr == NULL)
+	{
+		free(data.win_ptr);
+		return (MLX_ERROR);
+	}
+
+	/* Setup hooks  
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_key_hook(data.win_ptr, &handle_input, &data);
+
+	mlx_loop(data.mlx_ptr);
+
+	/* we will exit the loop if there's no window left, and execute this code 
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
+}
+*/
+#include <X11/X.h>
+#include <X11/keysym.h>
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_data;
+
+int	handle_no_event(void *data)
+{
+	/* This function needs to exist, but it is useless for the moment */
+	return (0);
+}
+
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	if (keysym == 119) //w = 119
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	printf("Keypress: %d\n", keysym);
+	return (0);
+}
+
+int	handle_keyrelease(int keysym, void *data)
+{
+	printf("Keyrelease: %d\n", keysym);
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+	if (data.win_ptr == NULL)
+	{
+		free(data.win_ptr);
+		return (MLX_ERROR);
+	}
+
+	/* Setup hooks */ 
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data); /* ADDED */
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data); /* CHANGED */
+
+	mlx_loop(data.mlx_ptr);
+
+	/* we will exit the loop if there's no window left, and execute this code */
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
--- So_long/Projetos-42/so_long/so_long/main.c
+++ So_long/Projetos-42/so_long/so_long/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tasantos <tasantos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 14:37:22 by tasantos          #+#    #+#             */
-/*   Updated: 2022/09/14 15:27:39 by tasantos         ###   ########.fr       */
+/*   Updated: 2022/09/19 11:08:09 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,17 +65,67 @@ typedef struct s_rect
 	int	color;
 }	t_rect;
 
+typedef struct s_map
+{
+	int	height;
+	int	width;
+}	t_map;
+
 int	key_hook(int keycode, t_data *data, void **mlx)
 {
 	printf("Keycode: %i\n", keycode);
 	return (0);
 }
 
+int	window_width(char	*path_map)
+{
+	int		file;
+	int		width;
+	char	*line;
+
+	width = 0;
+	file = open(path_map, O_RDONLY);
+	line = get_next_line(file);
+	while (ft_strchr(line, '1') == NULL)
+		line = get_next_line(file);
+	line = ft_strchr(line, '1');
+	width = ft_strlen(line);
+	return (width - 1);
+}
+
+int window_height(char	*path_map)
+{
+	int		file;
+	int		height;
+	char	*line;
+
+	height = 0;
+	file = open(path_map, O_RDONLY);
+	line = get_next_line(file);
+	while (ft_strchr(line, '1') == NULL)
+		line = get_next_line(file);
+	line = ft_strchr(line, '1');
+	while (line != NULL)
+	{
+		line = get_next_line(file);
+		height++;
+	}
+	return (height);
+}
+
 int main()
 {
 	t_data data;
     void    *mlx = mlx_init();
-    void    *window = mlx_new_window(mlx, 400, 400, "Capivarias");
+    void    *window;
+	
+	int     fd;
+    char    *line;
+	char	*result_start;
+	int		size;
+	int		lenght;
+	int		next_line;
+	int		first_line;
 
     int     x;
 	int		i;
@@ -95,10 +145,65 @@ int main()
 	void	*capy = mlx_xpm_file_to_image(mlx, "./images/sprites/capy.xpm", &c, &p);
 	void	*rock = mlx_xpm_file_to_image(mlx, "./images/backgrouds/Rock.xpm",&r, &k);
 
-	while (size_x < 32)
+	int		len;
+
+	t_map map;
+	map.width = window_width("./maps/map.ber");
+	map.height = window_height("./maps/map.ber");
+	window = mlx_new_window(mlx, map.width * 32, map.height * 32, "Capivarias");
+	
+	fd = open("./maps/map.ber", O_RDONLY);
+    line = get_next_line(fd);	
+	size = 0;
+	lenght = 0;
+	next_line = 0;
+	while (ft_strchr(line, '1') == NULL)
+		line = get_next_line(fd);
+	line = ft_strchr(line, '1');
+    if (line == NULL)
+		return (1);
+	result_start = malloc((ft_strlen(line) + 1));
+	ft_printf("O tamanho de line é: %i\n", ft_strlen(line));
+	first_line = ft_strlen(line);
+	while (line != NULL)
 	{
-		mlx_put_image_to_window(mlx, window, bkg_grass, size_x * 30, size_y * 30);
-		while (size_y < 32)
+		len = ft_strlen(line);
+		if (first_line != (len))
+			ft_printf("Error tamanho de linha!\n[first_line: %i]-[len: %i]\n", first_line, len);
+		while (size < len)
+		{
+			if (line[size] == '1' || line[size] == '0' || line[size] == 'C' || line[size] == 'P' || line[size] == 'E')
+			{
+				result_start[size] = line[size];
+				if (result_start[size] == '1')
+					mlx_put_image_to_window(mlx, window, rock, size * 32, next_line * 32);
+				else if (result_start[size] == 'P')
+					mlx_put_image_to_window(mlx, window, capy, size * 32, next_line * 32);
+				else if (result_start[size] == 'E')
+					mlx_put_image_to_window(mlx, window, bkg_water, size * 32, next_line * 32);
+				else if (result_start[size] == '0')
+					mlx_put_image_to_window(mlx, window, bkg_grass, size * 32, next_line * 32);
+				else if (result_start[size] == 'C')
+					mlx_put_image_to_window(mlx, window, img, size * 32, next_line * 32);
+			}
+			else 
+				ft_printf("\n\nERROR INVALID MAP size = %i\n\n", size);
+			size++;
+		}
+		result_start[size] = '\0';
+		ft_printf("[%i] %s\n", next_line, result_start);
+		size = 0;
+		line = get_next_line(fd);
+		next_line++;
+	}
+	ft_printf("O valor total de linhas é: %i\n", next_line);
+    mlx_loop(mlx);
+}
+
+/*	while (size_x < size)
+	{
+		mlx_put_image_to_window(mlx, window, bkg_grass, size_x * size, size_y * size);
+		while (size_y < size)
 		{
 			if (size_y == 7 && size_x == 7)
 				mlx_put_image_to_window(mlx, window, img, size_x * 30, size_y * 30);
@@ -125,9 +230,8 @@ int main()
 		size_y=0;
 		size_x++;
 	}
-    mlx_loop(mlx);
-}
-
+	*/
+/*
 int    map_validator()
 {
     int        fd;
@@ -138,32 +242,7 @@ int    map_validator()
 	int		next_line;
 
 
-	fd = open("./maps/map.ber", O_RDONLY);
-    line = get_next_line(fd);	
-	size = 0;
-	lenght = 0;
-	next_line = 0;
-    if (line == NULL)
-		return (1);
-	while (ft_strchr(line, '1') == NULL)
-		line = get_next_line(fd);
-	line = ft_strchr(line, '1');
-	result_start = malloc((ft_strlen(line) + 1));
-	ft_printf("O tamanho de line é: %i\n", ft_strlen(line));
-	while (line != NULL)
-	{
-		while (line[size] == '1' || line[size] == '0' || line[size] == 'C' || line[size] == 'P' || line[size] == 'E')
-		{
-			result_start[size] = line[size];
-			ft_printf("Result_Start [%c]\n", result_start[size]);
-			size++;
-		}
-		result_start[size] = '\0';
-		ft_printf("[%i] %s\n", next_line, result_start);
-		size = 0;
-		line = get_next_line(fd);
-		next_line++;
-	}
-	ft_printf("O valor total de linhas é: %i\n", next_line);
+
     return (0);
 }
+*/
